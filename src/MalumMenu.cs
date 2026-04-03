a61d350e4b49f8b620e22dda5b2133d6feba2b9d
scp222thj <82206202+scp222thj@users.noreply.github.com>
2026-04-03 11:23:10 +0200

    chore: add 2026.3.31 to supported AU versions

--- src/MalumMenu.cs
+++ src/MalumMenu.cs
@@ -27,7 +27,7 @@ public partial class MalumMenu : BasePlugin
     public static CheatToggles.KeybindListener keybindListener;
 
     public static string malumVersion = "3.0.2";
-    public static List<string> supportedAU = new List<string> { "2026.2.24", "2026.3.17" };
+    public static List<string> supportedAU = new List<string> { "2026.2.24", "2026.3.17", "2026.3.31" };
     public static bool isPanicked = false;
     public static bool inStealthMode = false;
 
